00ebd5e7b19ad7df8730242300e4921522c5fecf
filipkaduch <Madrid_Magduska2020>
2022-01-21 11:14:45 +0100

    url

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.get(/.*/, function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8000
 app.listen(port);
